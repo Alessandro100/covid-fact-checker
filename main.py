57418a5db8cf7f854e22e37d38d575528bb5b725
Abdulrahim Mansour <amansour@coveo.com>
2020-12-12 13:33:25 -0500

    implemented output files: trace & metrics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import math
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
 
 # No inputs
 # Outputs a 2d array, [class1, class2][class_prior, class_score, class_score_filtered]
@@ -52,7 +54,6 @@ def create_vocabulary():
                     word_dict[formatted_word] = word_dict[formatted_word] + 1
                 else:
                     word_dict[formatted_word] = 1
-
 
     class_1_filtered_word_dict = {key:val for key, val in class_1_word_dict.items() if val != 1}
     class_2_filtered_word_dict = {key:val for key, val in class_2_word_dict.items() if val != 1}
@@ -110,9 +111,9 @@ def create_vocabulary():
     score_class_2_filtered = train_nb_classifier(class_2_filtered_word_dict, filtered_vocabulary)
     
     nb_test(class_1_prior, score_class_1, class_1_word_dict,{1515155:['Trump', 'REEE', 'get', 'process', 'vaccine']}, original_vocabulary)
-    class_1_info = [class_1_prior, score_class_1, score_class_1_filtered, class_1_word_dict, class_1_filtered_word_dict,original_vocabulary]
-    class_2_info = [class_2_prior, score_class_2, score_class_2_filtered, class_2_word_dict, class_2_filtered_word_dict, filtered_vocabulary]
-    return [class_1_info, class_2_info]
+    class_1_info = [class_1_prior, score_class_1, score_class_1_filtered, class_1_word_dict, class_1_filtered_word_dict]
+    class_2_info = [class_2_prior, score_class_2, score_class_2_filtered, class_2_word_dict, class_2_filtered_word_dict]
+    return [class_1_info, class_2_info], original_vocabulary, filtered_vocabulary
 
 # Inputs are class dictionnary {w: # of appearences}
 # Output is the score of each word for the class {w: score_for_class}
@@ -142,27 +143,98 @@ def get_testing_set():
     with open("data/covid_test_public.tsv", encoding="utf-8") as fd:
         rd = csv.reader(fd, delimiter="\t", quotechar='"')
         for row in rd:
-            tokenised_tweets[row[0]] = row[1].split(" ")
+            tokenised_tweets[row[0]] = {'words': row[1].split(" "), 'correct_class': row[2]}
     return tokenised_tweets
+
+def trace_to_file(file_name, tweets):
+    with open(file_name, 'w') as f:
+        for tweet in tweets:
+            f.write(tweet['id'] + '  ' +
+                    tweet['predicted_class'] + '  ' +
+                    "{:.2e}".format(tweet['score']) + '  ' +
+                    tweet['correct_class'] + '  ' +
+                    tweet['outcome'] + '\n')
+
+def get_performance(file_name, tweets):
+    tweet_correct = np.array([])
+    tweet_predicted = np.array([])
+    true_postives = 0
+
+    for tweet in tweets:
+        tweet_correct = np.append(tweet_correct, tweet['correct_class'])
+        tweet_predicted = np.append(tweet_predicted, tweet['predicted_class'])
+        if (tweet['correct_class'] == tweet['predicted_class']):
+            true_postives = true_postives + 1
+
+    # precision: tp/tp+fp, recall: tp/tp+fn, f1: 2*precision*recall/precision+recall
+    stats = precision_recall_fscore_support(tweet_correct, tweet_predicted, average = None, labels = ['yes', 'no'])
+    accuracy = true_postives/len(tweets)
+    
+    print(accuracy, stats)
+
+
+    with open(file_name, 'w') as f:
+        f.write(str(accuracy) + '\n' +
+                str(stats[0][0]) + '  ' + str(stats[0][1]) + '\n' +
+                str(stats[1][0]) + '  ' + str(stats[1][1]) + '\n' +
+                str(stats[2][0]) + '  ' + str(stats[2][1]) + '\n')
     
 def main():
     
     tokenised_tweets_to_classify = get_testing_set() 
-    class_infos = create_vocabulary()   
-    print(tokenised_tweets_to_classify)
+    class_infos, original_vocabulary, filtered_vocabulary = create_vocabulary()  
+
+    original_results = []
+    filtered_results = []
     
     for tweet in tokenised_tweets_to_classify:
-       if(nb_test(class_infos[0][0],class_infos[0][1],  class_infos[0][3],tokenised_tweets_to_classify[tweet], class_infos[0][5] ) > nb_test(class_infos[1][0],class_infos[1][1], class_infos[1][3], tokenised_tweets_to_classify[tweet], class_infos[0][5])):
-           print("Tweet # ", tweet, "classified as verifiable factual information",nb_test(class_infos[0][0],class_infos[0][1], class_infos[0][3], tokenised_tweets_to_classify[tweet], class_infos[0][5]), "vs ", nb_test(class_infos[1][0],class_infos[1][1], class_infos[1][3] ,tokenised_tweets_to_classify[tweet], class_infos[0][5]) )
-       else: 
-           print("Tweet # ", tweet, "classified as non-verifiable factual information",nb_test(class_infos[0][0],class_infos[0][1], class_infos[0][3], tokenised_tweets_to_classify[tweet], class_infos[0][5]), "vs ", nb_test(class_infos[1][0],class_infos[1][1], class_infos[1][3] ,tokenised_tweets_to_classify[tweet], class_infos[0][5]) )
-    print("\n now for filtered vocabulary \n")      
-    for tweet in tokenised_tweets_to_classify:
-       if(nb_test(class_infos[0][0],class_infos[0][2],  class_infos[0][4],tokenised_tweets_to_classify[tweet], class_infos[1][5]) > nb_test(class_infos[1][0],class_infos[1][2], class_infos[1][4],tokenised_tweets_to_classify[tweet], class_infos[1][5])):
-           print("Tweet # ", tweet, "classified as verifiable factual information",nb_test(class_infos[0][0],class_infos[0][2], class_infos[0][4],tokenised_tweets_to_classify[tweet], class_infos[1][5]), "vs ", nb_test(class_infos[1][0],class_infos[1][2], class_infos[1][4],tokenised_tweets_to_classify[tweet], class_infos[1][5]) )
-       else: 
-           print("Tweet # ", tweet, "classified as non-verifiable factual information",nb_test(class_infos[0][0],class_infos[0][1], class_infos[0][3], tokenised_tweets_to_classify[tweet], class_infos[0][5]), "vs ", nb_test(class_infos[1][0],class_infos[1][1], class_infos[1][3] ,tokenised_tweets_to_classify[tweet], class_infos[0][5]) )
+        class1_score = nb_test(class_infos[0][0], class_infos[0][1], class_infos[0][3], tokenised_tweets_to_classify[tweet]['words'], original_vocabulary)
+        class2_score = nb_test(class_infos[1][0],class_infos[1][1], class_infos[1][3], tokenised_tweets_to_classify[tweet]['words'], original_vocabulary)
         
+        is_verifiable = False
+        if (class1_score > class2_score ):
+            is_verifiable = True        
+
+        predicted_class = 'yes' if is_verifiable else 'no'
+        correct_class = tokenised_tweets_to_classify[tweet]['correct_class']
+
+        original_results.append({
+            'id': tweet,
+            'predicted_class': predicted_class,
+            'score': class1_score if is_verifiable else class2_score,
+            'correct_class': correct_class,
+            'outcome': 'correct' if predicted_class == correct_class else 'wrong'
+        })
+    
+    for tweet in tokenised_tweets_to_classify:
+        class1_score = nb_test(class_infos[0][0], class_infos[0][2], class_infos[0][4], tokenised_tweets_to_classify[tweet]['words'], filtered_vocabulary)
+        class2_score = nb_test(class_infos[1][0],class_infos[1][2], class_infos[1][4], tokenised_tweets_to_classify[tweet]['words'], filtered_vocabulary)
+        
+        is_verifiable = False
+        if (class1_score > class2_score ):
+            is_verifiable = True  
+
+        predicted_class = 'yes' if is_verifiable else 'no'
+        correct_class = tokenised_tweets_to_classify[tweet]['correct_class']
+
+        filtered_results.append({
+            'id': tweet,
+            'predicted_class': predicted_class,
+            'score': class1_score if is_verifiable else class2_score,
+            'correct_class': correct_class,
+            'outcome': 'correct' if predicted_class == correct_class else 'wrong'
+        })
+
+    
+    #outputs
+    print("Original Vocabulary")      
+    trace_to_file('trace NB-BOW-OV.txt', original_results)
+    get_performance('eval NB-BOW-OV.txt', original_results)
+    
+    print("Filtered Vocabulary")      
+    trace_to_file('trace NB-BOW-FV.txt', filtered_results)
+    get_performance('eval NB-BOW-FV.txt', filtered_results)
+
 
 main()
 
